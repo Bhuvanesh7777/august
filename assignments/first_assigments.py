8de409bb1fdcf23cca5b4e4c109786c1807057d6
Bhuvanesh7777 <bhuvanesh.marolix@gmail.com>
2023-08-15 23:28:44 +0530

    creating assignments folder

---
+++ assignments/first_assigments.py
@@ -0,0 +1,37 @@
+# creating table using For and While loop
+#num=int(input('Enter a number :'))
+#for i in range(1,11):
+    #print(num,' x ',i,' = ',num*i)
+
+#num=int(input("enter a number:"))  
+#counter=1
+#while counter<=10:
+    #print(num,' x ',counter,'=',num*counter)
+    #counter+=1 
+
+
+# creating a logic using conditional statements
+#score=80
+#if score >=90:
+    #grade='a'
+#elif score >=70:
+   # grade='b'
+#elif score >=50:
+    #grade='c'
+#else:
+    #grade='f'
+#print("your grade is:", grade) 
+
+
+#creating a logic using logical and realtional operators
+
+passing_score=60
+science_score= int(input('enter your science score:'))
+math_score=int(input('enter your math score:'))
+if math_score>=passing_score and science_score>=passing_score:
+    print('pass')
+else:
+    print('fail')
+       
+
+
