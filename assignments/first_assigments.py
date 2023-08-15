68bdfadcf9b908eaeb0cce133bd277badd9a2fc3
Bhuvanesh7777 <142305769+Bhuvanesh7777@users.noreply.github.com>
2023-08-15 23:33:33 +0530

    Update first_assigments.py

--- assignments/first_assigments.py
+++ assignments/first_assigments.py
@@ -1,29 +1,29 @@
-# creating table using For and While loop
-#num=int(input('Enter a number :'))
-#for i in range(1,11):
-    #print(num,' x ',i,' = ',num*i)
+creating table using For and While loop
+num=int(input('Enter a number :'))
+for i in range(1,11):
+    print(num,' x ',i,' = ',num*i)
 
-#num=int(input("enter a number:"))  
-#counter=1
-#while counter<=10:
-    #print(num,' x ',counter,'=',num*counter)
-    #counter+=1 
-
-
-# creating a logic using conditional statements
-#score=80
-#if score >=90:
-    #grade='a'
-#elif score >=70:
-   # grade='b'
-#elif score >=50:
-    #grade='c'
-#else:
-    #grade='f'
-#print("your grade is:", grade) 
+num=int(input("enter a number:"))  
+counter=1
+while counter<=10:
+    print(num,' x ',counter,'=',num*counter)
+    counter+=1 
 
 
-#creating a logic using logical and realtional operators
+creating a logic using conditional statements
+score=80
+if score >=90:
+    grade='a'
+elif score >=70:
+   grade='b'
+elif score >=50:
+    grade='c'
+else:
+    grade='f'
+print("your grade is:", grade) 
+
+
+creating a logic using logical and realtional operators
 
 passing_score=60
 science_score= int(input('enter your science score:'))
