a4a4224b6c1f0e1bc18e0b3bdf33b0128deffd6e
Bhuvanesh7777 <bhuvanesh.marolix@gmail.com>
2023-09-13 19:55:04 +0530

    reduce function

---
+++ assignments/reduce.py
@@ -0,0 +1,15 @@
+#from functools import*
+#program to product all the elements in a list
+#l=eval(input('enter a elements: '))
+#product =reduce(lambda a,b : a*b,l)
+#print(product)
+
+#program to find the maximum values in the list
+#numbers=eval(input("enter the elements:"))
+#max_value=reduce(lambda a,b : a if a>b else b , numbers)
+#print(max_value)
+
+#program to find sum of digits in the list
+#numbers=eval(input("Enter a elements:"))
+#sum=reduce(lambda a,b: a+b, numbers)
+#print(sum)
