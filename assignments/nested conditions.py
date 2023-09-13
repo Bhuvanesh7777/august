01b2491418a1e4fc9e66f182f44b61d97e94c7d2
Bhuvanesh7777 <bhuvanesh.marolix@gmail.com>
2023-09-13 20:02:33 +0530

    nested conditions

---
+++ assignments/nested conditions.py
@@ -0,0 +1,32 @@
+#nested function to get numbers multiplied by 3and divided by 3 of given number
+#def mul(x,y):
+#    print(f'multiplied by 3 = {x*3,y*3}')
+#    def div(x,y):
+#        print(f'divided by 3 = {x/3,y/3}')
+#   div(x,y)
+#x=int(input('enter a number 1: '))
+#y=int(input('enter a number 2: '))
+#mul(x,y)
+
+#nested function to get sum and product of given numbers
+#def sum(x,y):
+#    print(f'sum = {x+y}')
+ #   def pro(x,y):
+ #       print(f'pro = {x*y}')
+ #   pro(x,y)
+
+#x=int(input('enter a number 1: '))
+#y=int(input('enter a number 2: '))
+#sum(x,y)
+
+#nested function to get greater and lowest of given numbers')
+#def hig(x,y):
+#    print(f'greater = {x if x>y else y}')
+#    def low(x,y):
+ #       print(f'lowest = {x if x<y else y}')
+ #   low(x,y)
+
+#x=int(input('enter a number 1: '))
+#y=int(input('enter a number 2: '))
+#hig(x,y)
+
